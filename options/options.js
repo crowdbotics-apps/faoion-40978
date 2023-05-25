6b12ea0bc4f721f8bb5f0669a36ce2645bc12895
Faoion lnc. <faoionlnc@gmail.com>
2023-05-25 12:13:21 +0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "faoion-40978",
-  url: "https://faoion-40978.botics.co",
-  api: "https://faoion-40978.botics.co/api/v1"
-}
+  "name": "faoion-40978",
+  "url": "https://faoion-40978.botics.co",
+  "api": "https://faoion-40978.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
