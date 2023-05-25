aa68e525156bc981986661418a75d0cb5f71b3c9
Crowdbotics <team@crowdbotics.com>
2023-05-25 05:12:51 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/faoion_40978/MainActivity.java
@@ -0,0 +1,15 @@
+package com.faoion_40978;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "faoion_40978";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "faoion-40978",
+  url: "https://faoion-40978.botics.co",
+  api: "https://faoion-40978.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
